6bc90c56b430b8be3ebeb5b6f4c312e0887ee7d4
Alex Cizinsky <alex.cizinsky@gmail.com>
2023-08-15 23:07:34 +0200

    clang-format for prevous commit

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,14 @@
 void initColors();
 
 int main(int argc, char** argv) {
-    const int playRows = 4; 
+    const int playRows = 4;
     const int playCollumns = 4;
-    const int tileHeigth = 3; 
-    const int tileWidth = 7;  
-    const int boardStartingRow = 2;     // y, starting row on the screen
-    const int boardStartingCollumn = 1; // x, starting collumn on the screen                            
+    const int tileHeigth = 3;
+    const int tileWidth = 7;
+    const int boardStartingRow = 2;             // y, starting row on the screen
+    const int boardStartingCollumn = 1;         // x, starting collumn on the screen
     const unsigned char startupProbability = 6; // pobability of new "2" appering on starup
-    const unsigned char moveProbability = 12;  // and every move
+    const unsigned char moveProbability = 12;   // and every move
 
     bool gameIsRunning = 1;
 
