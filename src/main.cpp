6e5747b4bad10556d0aab4353147ca82212d023a
Alex Cizinsky <alex.cizinsky@gmail.com>
2023-08-19 10:12:40 +0200

    check for commandline parameters only if there are any (argc > 1)

--- src/main.cpp
+++ src/main.cpp
@@ -25,38 +25,38 @@ int main(int argc, const char* argv[]) {
     const unsigned char moveProbability = 12;   // and every move
 
     // commandline arguments
-    argumentParser argParser(argc, argv);
+    argumentParser argParser(argc, argv); // I cannot declare it in the if statement, because it is a different scope
+    if (argc > 1) {
+        if (!argParser.setIntToOption("-r", &playRows, {3, 100}))
+            return 0;
+        if (!argParser.setIntToOption("--rows", &playRows, {3, 100}))
+            return 0;
+        if (!argParser.setIntToOption("-c", &playCollumns, {3, 100}))
+            return 0;
+        if (!argParser.setIntToOption("--columns", &playCollumns, {3, 100}))
+            return 0;
+        if (!argParser.setIntToOption("--tile-width", &tileWidth, {5, 15}))
+            return 0;
+        if (!argParser.setIntToOption("--tile-height", &tileHeigth, {3, 10}))
+            return 0;
 
-    if (!argParser.setIntToOption("-r", &playRows, {3, 100}))
-        return 0;
-    if (!argParser.setIntToOption("--rows", &playRows, {3, 100}))
-        return 0;
-    if (!argParser.setIntToOption("-c", &playCollumns, {3, 100}))
-        return 0;
-    if (!argParser.setIntToOption("--columns", &playCollumns, {3, 100}))
-        return 0;
-    if (!argParser.setIntToOption("--tile-width", &tileWidth, {5, 15}))
-        return 0;
-    if (!argParser.setIntToOption("--tile-height", &tileHeigth, {3, 10}))
-        return 0;
-
-    if (argParser.optionExists("--help")) {
-        std::cout << "Usage: terminal-2048 [options] \n\n Options: \n"
-                  << " -r --rows      NUMBER  Set How many rows the board has, range: 3, 100\n"
-                  << " -c --columns   NUMBER  Set How many columns the board has, range: 3, 100\n"
-                  << " --tile-width   NUMBER  Set How wide is single tile on the board in characters, range: 5, 15\n"
-                  << " --tile-height  NUMBER  Set How high is single tile on the board in rows, range: 3, 10\n"
-                  << " --no-color             Don't use colors\n"
-                  << " --help                 Show this help\n";
-        return 0;
+        if (argParser.optionExists("--help")) {
+            std::cout << "Usage: terminal-2048 [options] \n\n Options: \n"
+                      << " -r --rows      NUMBER  Set How many rows the board has, range: 3, 100\n"
+                      << " -c --columns   NUMBER  Set How many columns the board has, range: 3, 100\n"
+                      << " --tile-width   NUMBER  Set How wide is single tile on the board in characters, range: 5, 15\n"
+                      << " --tile-height  NUMBER  Set How high is single tile on the board in rows, range: 3, 10\n"
+                      << " --no-color             Don't use colors\n"
+                      << " --help                 Show this help\n";
+            return 0;
+        }
     }
-
     // ncurses initialization
     initscr();
     noecho();
     keypad(stdscr, TRUE);
 
-    if (argParser.optionExists("--no-color")) {
+    if (argc > 0 && argParser.optionExists("--no-color")) {
         useColor = false;
     } else {
         useColor = has_colors();
