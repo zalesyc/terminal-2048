590791cc5521d0c65431cda082bdcbb7d42f7790
Alex Cizinsky <alex.cizinsky@gmail.com>
2023-09-07 20:17:19 +0200

    renamed headers from .h to .hpp

---
+++ src/popup.hpp
@@ -0,0 +1,30 @@
+#ifndef POPUP_H
+#define POPUP_H
+
+#include <ncurses.h>
+#include <string>
+#include <vector>
+
+#include "game.hpp"
+#include "tile.hpp"
+
+class Popup {
+  public:
+    Popup(App* appconfig, std::vector<std::vector<Tile>>* board);
+    ~Popup();
+
+    void setText(std::string text);
+
+  private:
+    std::string print(int width);
+
+  private:
+    WINDOW* m_win;
+    std::string m_text;
+    App* m_appconfig;
+    std::vector<std::vector<Tile>>* m_board;
+    int m_winWidth;
+    int m_winHeight;
+};
+
+#endif // POPUP_H
